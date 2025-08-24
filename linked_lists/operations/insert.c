fc6ecb4451d6f6b89bcec95c1dd54bf2f72944d0
gian.gg <gianepanto@gmail.com>
2025-08-24 15:41:06 +0800

    ll insert

--- linked_lists/operations/insert.c
+++ linked_lists/operations/insert.c
@@ -6,72 +6,92 @@ typedef struct node {
     struct node *link;
 } *LIST;
 
-void init(LIST *HEAD) {
-    *HEAD = NULL;
-}
-void display(char string[], LIST HEAD) {
-    printf("%s", string);
-    for(LIST curr = HEAD; curr != NULL; curr = curr->link) {
-        printf("%d ", curr->data);
-    }
-    printf("\n");
-}
-LIST newNode(int data, struct node *link) {
-    LIST temp = malloc(sizeof(struct node));
-    if(temp == NULL) {
-        printf("\nMALLOC FAILED!\n");
-        exit(1);
-    }
+void init(LIST*);
+void display(char[], LIST);
+void insertEnd(LIST*, int);
+void insertStart(LIST*, int);
+void insertPos(LIST*, int, int);
+void insertSort(LIST*, int);
 
-    temp->data = data;
-    temp->link = link;
-
-    return temp;
-}
-// inserts data at the end
-void insert_end(LIST *HEAD, int data) {
-    LIST *curr;
-    for(curr = HEAD; (*curr) != NULL; curr = &(*curr)->link) {}
-    *curr = newNode(data, NULL);
-}
-// inserts data at the start
-void insert_start(LIST *HEAD, int data) {
-    *HEAD = newNode(data, *HEAD);
-}
-// inserts data at the given index (starts from 0)
-void insert(LIST *HEAD, int index, int data) {
-    LIST *curr;
-    int i = 0;
-    for(curr = HEAD; ((*curr) != NULL) && (i < index); curr = &(*curr)->link, i++) {}
-    *curr = newNode(data, *curr);
-}
-// intelligently insert new data ascendingly
-void insert_sorted(LIST *HEAD, int data) {
-    LIST *curr;
-    for(curr = HEAD; (*curr)!=NULL && data > (*curr)->data;curr=&(*curr)->link) {}
-    *curr = newNode(data, *curr);
-}
-
-int main() {
+int main () {
     LIST HEAD;
     init(&HEAD);
 
-    insert_end(&HEAD, 200);
-    insert_end(&HEAD, 30);
-    insert_end(&HEAD, 500);
-    insert_end(&HEAD, 2000);
-    insert_end(&HEAD, 2000);
-    insert_end(&HEAD, 2000);
-    insert_end(&HEAD, 2000);
-    insert_end(&HEAD, 2000);
+    insertEnd(&HEAD, 10);
+    insertEnd(&HEAD, 20);
+    insertEnd(&HEAD, 30);
+    insertEnd(&HEAD, 40);
+    insertEnd(&HEAD, 50);
 
-    display("LIST: ", HEAD);
+    display("Before: ", HEAD);
 
-    insert(&HEAD, 1, 99);
-    insert(&HEAD, 8, 88);
+    insertPos(&HEAD, 35, 1);
 
-    display("LIST: ", HEAD);
-
+    display("After: ", HEAD);
 
     return 0;
+}
+
+void init(LIST *HEAD) {
+    *HEAD = NULL;
+}
+
+void display(char string[], LIST HEAD) {
+    printf("%s", string);
+    for(LIST curr = HEAD; curr!= NULL; curr=curr->link) printf("%d ", curr->data);
+    printf("\n");
+}
+
+void insertEnd(LIST *HEAD, int data) {
+    LIST *curr;
+    for(curr = HEAD; (*curr)!=NULL; curr = &(*curr)->link);
+
+    LIST newNode = malloc(sizeof(struct node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->link = NULL;
+        *curr = newNode;
+    } else {
+        printf("Malloc Failed!");
+    }
+}
+
+void insertStart(LIST *HEAD, int data) {
+    LIST newNode = malloc(sizeof(struct node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->link = *HEAD;
+        *HEAD = newNode;
+    } else {
+        printf("Malloc Failed!");
+    }
+}
+
+void insertPos(LIST *HEAD, int data, int index) {
+    LIST *curr;
+    int i = 0;
+    for(curr = HEAD; (*curr)!=NULL && i < index; curr = &(*curr)->link, i++);
+
+    LIST newNode = malloc(sizeof(struct node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->link = *curr;
+        *curr = newNode;
+    } else {
+        printf("Malloc Failed!");
+    }
+}
+
+void insertSort(LIST *HEAD, int data) {
+    LIST *curr;
+    for(curr = HEAD; (*curr)!=NULL && data > (*curr)->data; curr = &(*curr)->link);
+
+    LIST newNode = malloc(sizeof(struct node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->link = *curr;
+        *curr = newNode;
+    } else {
+        printf("Malloc Failed!");
+    }
 }
