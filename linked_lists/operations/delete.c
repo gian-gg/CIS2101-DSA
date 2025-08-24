2a1bb2fc05a00b64e45af86cf6879250f8b0ed27
gian.gg <gianepanto@gmail.com>
2025-08-24 16:26:51 +0800

    ll: delete

--- linked_lists/operations/delete.c
+++ linked_lists/operations/delete.c
@@ -6,99 +6,109 @@ typedef struct node {
     struct node *link;
 } *LIST;
 
-void init(LIST *HEAD) {
+void init(LIST*);
+void display(char[], LIST);
+void insertEnd(LIST*,int);
+
+void delete(LIST*, int);
+void deleteElem(LIST*, int);
+void deleteAllOccur(LIST*, int);
+void deleteAll(LIST*);
+
+int main() {
+    LIST HEAD;
+    init(&HEAD);
+
+    insertEnd(&HEAD, 50);
+    insertEnd(&HEAD, 10);
+    insertEnd(&HEAD, 50);
+    insertEnd(&HEAD, 20);
+    insertEnd(&HEAD, 30);
+    insertEnd(&HEAD, 40);
+    insertEnd(&HEAD, 50);
+    insertEnd(&HEAD, 50);
+
+    display("Before:\t", HEAD);
+
+    deleteAllOccur(&HEAD, 50);
+
+    display("After:\t", HEAD);
+
+    return 0;
+}
+
+void init(LIST* HEAD) {
     *HEAD = NULL;
 }
-void display(char string[], LIST HEAD) {
-    printf("%s", string);
-    for(LIST curr = HEAD; curr != NULL; curr = curr->link) {
-        printf("%d ", curr->data);
-    }
-    printf("\n");
-}
-void insert_end(LIST *HEAD, int data) {
-    LIST *curr;
-    for(curr = HEAD; (*curr) != NULL; curr = &(*curr)->link);
 
-    LIST temp = malloc(sizeof(struct node));
-    if (temp == NULL) {
-        printf("\nMALLOC FAILED!\n");
+void display(char string[], LIST HEAD) {
+    if (HEAD == NULL) {
+        printf("\nList is Empty!\n");
         return;
     }
 
-    temp->data = data;
-    temp->link = NULL;
-
-    (*curr) = temp;
+    printf("%s", string);
+    for(LIST curr = HEAD; curr!=NULL; curr=curr->link) printf("%d ", curr->data);
+    printf("\n");
 }
 
-void delete(LIST *HEAD, int index) {
+void insertEnd(LIST* HEAD,int data) {
+    LIST *curr;
+    for(curr = HEAD; (*curr)!=NULL; curr = &(*curr)->link);
+
+    LIST newNode = malloc(sizeof(struct node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->link = NULL;
+        *curr = newNode;
+    } else {
+        printf("MALLOC FAILED!");
+    }
+}
+
+void delete(LIST* HEAD, int index) {
     LIST *curr;
     int i = 0;
-    for(curr=HEAD; (*curr)!=NULL && (i > index-1); curr=&(*curr)->link,i++);
+    for(curr = HEAD; (*curr)!=NULL && i < index; curr = &(*curr)->link, i++);
 
-    LIST temp = *curr;
-    (*curr) = (*curr)->link;
-
-    free(temp);
+    if (*curr!=NULL) {
+        LIST temp = *curr;
+        *curr = temp->link;
+        free(temp);
+    }
 }
 
-void delete_all(LIST *HEAD) {
-    LIST *curr = HEAD;
-    while((*curr)!=NULL) {
-        LIST temp = *curr;
-        curr=&(*curr)->link;
-
+void deleteAll(LIST* HEAD) {
+    LIST curr = *HEAD;
+    while(curr!=NULL) {
+        LIST temp = curr;
+        curr = curr->link;
         free(temp);
     }
 
     init(HEAD);
 }
 
-void deleteElem(LIST *HEAD, int elem) {
+void deleteElem(LIST* HEAD, int elem) {
     LIST *curr;
-    for(curr = HEAD; (*curr) != NULL && (*curr)->data != elem; curr = &(*curr)->link);
+    for(curr = HEAD; *curr!=NULL && elem != (*curr)->data; curr=&(*curr)->link);
 
-    if ((*curr) != NULL) {
+    if (*curr!=NULL) {
         LIST temp = *curr;
-        (*curr) = (*curr)->link;
-
+        *curr = temp->link;
         free(temp);
     }
 }
 
-void deleteAllOccur(LIST *HEAD, int elem) {
+void deleteAllOccur(LIST* HEAD, int elem) {
     LIST *curr = HEAD;
-    while((*curr) != NULL) {
-        if ((*curr)->data == elem) {
+    while(*curr!=NULL) {
+        if (elem == (*curr)->data) {
             LIST temp = *curr;
-            (*curr) = (*curr)->link;
+            *curr = temp->link;
             free(temp);
         } else {
             curr = &(*curr)->link;
         }
     }
-}
-
-int main() {
-    LIST HEAD;
-    init(&HEAD);
-
-    insert_end(&HEAD, 10);
-    insert_end(&HEAD, 50);
-    insert_end(&HEAD, 50);
-    insert_end(&HEAD, 30);
-    insert_end(&HEAD, 40);
-    insert_end(&HEAD, 50);
-    insert_end(&HEAD, 50);
-    insert_end(&HEAD, 50);
-
-
-    display("LIST:\t", HEAD);
-
-    deleteAllOccur(&HEAD, 50);
-
-    display("LIST:\t", HEAD);
-
-    return 0;
 }
