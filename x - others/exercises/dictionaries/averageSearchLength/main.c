a169e84f5bb55a548da7b8bff4d714c18da5b653
gian.gg <gianepanto@gmail.com>
2025-09-29 06:31:53 +0800

    dicts(ex): average search length

---
+++ x - others/exercises/dictionaries/averageSearchLength/main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 10
+
+typedef char DICTIONARY[MAX];
+
+#define EMPTY   -1
+#define DELETED -2
+
+
+int hash(char); // returns the hash value of the given value
+void init(DICTIONARY); // initializes the DICTIONARY (makeNull)
+int insert(DICTIONARY, char); // inserts an element
+void delete(DICTIONARY, char); // deletes the given element
+bool member(DICTIONARY, char); // returns true if an element is a member of the given DICTIONARY, otherwise false
+
+void visualize(DICTIONARY); // visualize the elements
+void display(DICTIONARY); // displays the elements side-by-side
+
+int searchLength(int hashVal, int actualLoc) {
+    return ((actualLoc - hashVal + MAX) % MAX) + 1;
+}
+
+int main() {
+    DICTIONARY D;
+    init(D);
+
+    char elements[] = {'A','B','C','D','E','F','G','H'};
+    int n = sizeof(elements) / sizeof(elements[0]);
+    int totalSL = 0;
+
+    printf("Insertion and Search Lengths:\n");
+    for (int i = 0; i < n; i++) {
+        char elem = elements[i];
+        int hv = hash(elem);
+        int loc = insert(D, elem);
+        int SL = searchLength(hv, loc);
+        printf("Element %c → Hash=%d, Location=%d, SL=%d\n", elem, hv, loc, SL);
+        totalSL += SL;
+    }
+
+    double avgSL = (double) totalSL / n;
+    printf("\nAverage Search Length = %.3f\n\n", avgSL);
+
+    printf("Final Dictionary State:\n");
+    visualize(D);
+
+    return 0;
+}
+
+int hash(char data) {
+    switch(data) {
+        case 'A': return 1;
+        case 'B': return 4;
+        case 'C': return 9;
+        case 'D': return 9;
+        case 'E': return 0;
+        case 'F': return 3;
+        case 'G': return 4;
+        case 'H': return 3;
+        default: return -1;
+    }
+}
+
+void init(DICTIONARY D) {
+    for(int idx = 0; idx < MAX; idx++) {
+        D[idx] = EMPTY;
+    }
+}
+
+int insert(DICTIONARY D, char data) {
+    int hashValue = hash(data);
+
+    int idx, trav;
+    for (idx = 0, trav = hashValue; idx < MAX && D[trav] != EMPTY && D[trav] != DELETED; idx++, trav = (hashValue + idx) % MAX) {}
+
+    if (idx < MAX) {
+        D[trav] = data;
+        return trav;
+    } else {
+        printf("Hash table full! Cannot insert %c\n", data);
+        return -1;
+    }
+}
+
+void delete(DICTIONARY D, char data) {
+    int hashValue = hash(data);
+
+    int idx, trav;
+    for (idx = 0, trav = hashValue; idx < MAX && D[trav] != EMPTY && D[trav] != data; idx++, trav = (hashValue + idx) % MAX) {}
+
+    if (idx < MAX && D[trav] == data) {
+        D[trav] = DELETED;
+    } else {
+        printf("%c not found in dictionary\n", data);
+    }
+}
+
+bool member(DICTIONARY D, char data) {
+    int hashValue = hash(data);
+
+    int idx, trav;
+    for (idx = 0, trav = hashValue; idx < MAX && D[trav] != EMPTY && D[trav] != data; idx++, trav = (hashValue + idx) % MAX) {}
+
+    return (idx < MAX && D[trav] == data) ? true : false;
+}
+
+void visualize(DICTIONARY D) {
+    for (int idx = 0; idx < MAX; idx++) {
+        char currElem = D[idx];
+        
+        if (currElem == EMPTY) {
+            printf("[ %3s ]\n", "EMP");
+        } else if (currElem == DELETED) {
+            printf("[ %3s ]\n", "DEL");
+        } else {
+            printf("[ %3c ]\n", currElem);
+        }
+    }
+    printf("\n");
+}
+
+void display(DICTIONARY D) {
+    for (int idx = 0; idx < MAX; idx++) {
+        char currElem = D[idx];
+
+        if (currElem != EMPTY && currElem != DELETED) {
+            printf("%c ", currElem);
+        }
+    }
+    printf("\n");
+}
